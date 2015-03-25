7e23a9f16d78414a9c64249a7d9f1d793f0fc5c6
Misuke <misuke01@gmail.com>
2015-03-25 09:38:31 +0100

    add card model

---
+++ Modele_de_jeu/src/jeu/model/Card.java
@@ -0,0 +1,15 @@
+package jeu.model;
+
+import javax.swing.*;
+
+/**
+ * @author Misuke
+ * @version 25/03/15
+ */
+public class Card extends JButton {
+    ImageIcon image;
+
+    public Card(ImageIcon image) {
+        this.image = image;
+    }
+}
