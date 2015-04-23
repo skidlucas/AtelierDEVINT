5c59596454b65218fa7be1a8426e7fdb921c697d
Misuke <misuke01@gmail.com>
2015-04-23 10:45:58 +0200

    branch scores

---
+++ Modele_de_jeu/src/jeu/scores/Score.java
@@ -0,0 +1,16 @@
+package jeu.scores;
+
+/**
+ * Created by user on 23/04/2015.
+ */
+public class Score {
+    private String name;
+    private int nbPoint;
+    private int time;
+
+    public Score(String name, int nbPoint, int time) {
+        this.name = name;
+        this.nbPoint = nbPoint;
+        this.time = time;
+    }
+}
---
+++ Modele_de_jeu/src/jeu/scores/ScoreManager.java
@@ -0,0 +1,10 @@
+package jeu.scores;
+
+import java.util.List;
+
+/**
+ * Created by user on 23/04/2015.
+ */
+public class ScoreManager {
+    private List<Score> scores;
+}
